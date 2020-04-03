68cdd80f3c01312f6411ea3d23904ff2cd3c38ba
Alejandro Beltran <achavez_97@hotmail.com>
2020-04-02 21:19:42 -0600

    Prueba de Commit

--- app/src/main/java/com/example/proyecto1cm/MainActivity.java
+++ app/src/main/java/com/example/proyecto1cm/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.proyecto1cm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout myLayout;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myLayout = (ConstraintLayout) findViewById(R.id.myLayout);
+
+        animationDrawable = (AnimationDrawable) myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
     }
 }
