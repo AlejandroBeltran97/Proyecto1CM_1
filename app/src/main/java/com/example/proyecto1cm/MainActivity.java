3281afe7dd9940b6bb01e0ff457fd2989ba18899
Alejandro Beltran <achavez_97@hotmail.com>
2020-04-03 02:39:43 -0600

    Inserción del campo del nombre, apellidos y prueba de la fecha.

--- app/src/main/java/com/example/proyecto1cm/MainActivity.java
+++ app/src/main/java/com/example/proyecto1cm/MainActivity.java
@@ -6,10 +6,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout myLayout;
     AnimationDrawable animationDrawable;
+
+    private TextInputEditText get_name;
+    private TextInputEditText get_apellidos;
+    private TextInputLayout name_input;
+    private TextInputLayout last_name_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,11 @@ public class MainActivity extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(4500);
         animationDrawable.setExitFadeDuration(4500);
         animationDrawable.start();
+
+        get_name = (TextInputEditText) findViewById(R.id.get_name);
+        get_apellidos = (TextInputEditText) findViewById(R.id.get_apellidos);
+        name_input = (TextInputLayout) findViewById(R.id.name_input);
+        last_name_input = (TextInputLayout) findViewById(R.id.last_name_input);
     }
 }
+
