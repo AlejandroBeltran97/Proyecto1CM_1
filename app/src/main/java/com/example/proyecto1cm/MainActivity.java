02edcff761558e8720f6386dad5450819a2b004a
Alejandro Beltran <achavez_97@hotmail.com>
2020-04-06 03:28:59 -0500

    Modificación de los campos de Nombre, Apellidos y Fecha de Nacimiento.

--- app/src/main/java/com/example/proyecto1cm/MainActivity.java
+++ app/src/main/java/com/example/proyecto1cm/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.proyecto1cm;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -9,10 +16,16 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout myLayout;
     AnimationDrawable animationDrawable;
+
+    private static final String TAG = "MainActivity";
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     private TextInputEditText get_name;
     private TextInputEditText get_apellidos;
@@ -25,16 +38,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myLayout = (ConstraintLayout) findViewById(R.id.myLayout);
+        get_name = (TextInputEditText) findViewById(R.id.get_name);
+        get_apellidos = (TextInputEditText) findViewById(R.id.get_apellidos);
+        name_input = (TextInputLayout) findViewById(R.id.name_input);
+        last_name_input = (TextInputLayout) findViewById(R.id.last_name_input);
 
         animationDrawable = (AnimationDrawable) myLayout.getBackground();
         animationDrawable.setEnterFadeDuration(4500);
         animationDrawable.setExitFadeDuration(4500);
         animationDrawable.start();
 
-        get_name = (TextInputEditText) findViewById(R.id.get_name);
-        get_apellidos = (TextInputEditText) findViewById(R.id.get_apellidos);
-        name_input = (TextInputLayout) findViewById(R.id.name_input);
-        last_name_input = (TextInputLayout) findViewById(R.id.last_name_input);
+        mDisplayDate = (TextView) findViewById(R.id.birth);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        R.style.dateTheme,
+                        mDateSetListener,
+                        year, month, day);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: date: " + year + "/" + month + "/" + "dayOfMonth");
+                String date = day + "/" + month + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
     }
 }
 
